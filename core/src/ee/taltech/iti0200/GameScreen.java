7e4839fe77dba47435b53efcebd664fea3d46675
pikoiv <pikoiv@ttu.ee>
2020-02-26 22:19:17 +0200

    Issue 6 Jumping

--- core/src/ee/taltech/iti0200/GameScreen.java
+++ core/src/ee/taltech/iti0200/GameScreen.java
@@ -5,34 +5,48 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class GameScreen implements Screen {
     private SpaceEscape game;
+    private List<Player> players;
+    private List<Enemy> enemies;
     private Player player;
-    private Physics physics;
+    private Enemy enemy;
 
     private OrthographicCamera camera;
 
     public GameScreen(SpaceEscape game) {
         this.game = game;
-        this.physics = new Physics();
 
         this.player = new Player(Gdx.files.internal("box.jpg"), 50, 50);
+        this.enemy = new Enemy(Gdx.files.internal("enemy.jpg"), 50, 50);
         this.camera = new OrthographicCamera();
         this.camera.setToOrtho(false, 1800, 900);
+
+        players = new LinkedList<>();
+        players.add(player);
+        enemies = new LinkedList<>();
+        enemies.add(enemy);
     }
 
     @Override
     public void render(float delta) {
+        game.batch.begin();
         camera.update();
         game.batch.setProjectionMatrix(camera.combined);
-
-        physics.move(player);
-        player.boundsLeftAndRight(camera);
-        player.boundsUpAndDown(camera);
+        for (Player player1 : players) {
+            player1.move();
+            player1.boundsLeftAndRight(camera);
+            player1.boundsUpAndDown(camera);
+        }
+        enemy.boundsLeftAndRight(camera);
+        enemy.boundsUpAndDown(camera);
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        game.batch.begin();
         game.batch.draw(player.getCharacterImage(), player.getObject().getX(), player.getObject().getY());
+        game.batch.draw(enemy.getCharacterImage(), enemy.getObject().getX(), enemy.getObject().getY());
         game.batch.end();
     }
 
