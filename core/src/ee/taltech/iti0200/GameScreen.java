dfcf81b877e8d94c5929a0772cb18bd908e64484
Minwas <you@example.com>
2020-04-09 12:16:48 +0300

    issue 37

--- core/src/ee/taltech/iti0200/GameScreen.java
+++ core/src/ee/taltech/iti0200/GameScreen.java
@@ -12,6 +12,8 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Action;
+import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
@@ -19,13 +21,16 @@ import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import ee.taltech.iti0200.entities.Player;
+import ee.taltech.iti0200.entities.Entity;
 import ee.taltech.iti0200.entities.PlayerType;
 import ee.taltech.iti0200.world.GameMap;
 import ee.taltech.iti0200.world.TiledGameMap;
 import org.w3c.dom.Text;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class GameScreen implements Screen {
     private SpaceEscape game;
@@ -33,24 +38,36 @@ public class GameScreen implements Screen {
 
     // stage
     public Stage stage;
-    public float deltaTime;
-    public String timeStr = "0";
+    public float deltaTime, currentTime, xCoolDownPoint, cCoolDownPoint, vCoolDownPoint;
+    public String timeStr = "0", xLabelText, cLabelText, vLabelText;
     public BitmapFont font;
     public Label.LabelStyle labelStyle;
-    public Label label;
-    public Image xSkill;
-    public Image cSkill;
-    public Image vSkill;
+    public Label label, cLabel, vLabel, xLabel, zLabel;
+    public Image xSkill, cSkill, vSkill, zSkill;
+    public Texture xSkillTexture, cSkillTexture, vSkillTexture, zSkillTexture, corpseTexture;
+    public int xCooldownTime, cCooldownTime, vCooldownTime, shakeIntensityRange = 30, shakeIntensityBuffer = 15;
+    public boolean shaking, gonnaShake;
+    public Random random;
+    public double shakingTime = 0.15;
+    public List<Entity> dead;
+
+
 
 
     // cooldowns
-    public float currentTime;
-    public float xCooldownTime = 2;
-    public float xCoolDownPoint = 0;
-    public int cCooldownTime = 3;
-    public float cCoolDownPoint;
-    public int vCooldownTime = 5;
-    public float vCoolDownPoint;
+//    public float currentTime;
+//    public float xCooldownTime = 4;
+//    public float xCoolDownPoint = 0;
+//    public String xLabelText = "";
+//    public int cCooldownTime = 3;
+//    public float cCoolDownPoint;
+//    public String cLabelText = "";
+//    public int vCooldownTime = 5;
+//    public float vCoolDownPoint;
+//    public String vLabelText = "";
+//    public int zCooldownTime = 4;
+//    public float zCoolDownPoint;
+//    public String zLabelText = "";
 
 
     public OrthographicCamera camera;
@@ -70,11 +87,13 @@ public class GameScreen implements Screen {
         stage = new Stage(new ScreenViewport());
         font = new BitmapFont();
         labelStyle = new Label.LabelStyle(font, Color.WHITE);
-        Label.LabelStyle labelStyleTwo = new Label.LabelStyle(font, Color.BLACK);
+        Label.LabelStyle labelStyleTwo = new Label.LabelStyle(font, Color.RED);
 
         label = new Label(timeStr, labelStyle);
         label.setPosition(Gdx.graphics.getWidth() - 60, Gdx.graphics.getHeight() - 50);
         stage.addActor(label);
+
+        corpseTexture = new Texture("corpse.png");
 
         // --------------   DRAWING  ------------
 
@@ -83,24 +102,99 @@ public class GameScreen implements Screen {
         int screenCenterY = Gdx.graphics.getHeight() / 2;
 
         // abilities
-        Texture xSkillTexture = new Texture("PlayerAbilities/xSkill.png");
-        Texture cSkillTexture = new Texture("PlayerAbilities/cSkill.png");
-        Texture vSkillTexture = new Texture("PlayerAbilities/vSkill.png");
+        if (playerType == PlayerType.PLAYER0) {
+            zSkillTexture = new Texture("PlayerAbilities/Player0/zSkill.png");
+            xSkillTexture = new Texture("PlayerAbilities/Player0/xSkill.png");
+            cSkillTexture = new Texture("PlayerAbilities/Player0/cSkill.png");
+            vSkillTexture = new Texture("PlayerAbilities/Player0/vSkill.png");
+
+            xCooldownTime = 4;
+            xLabelText = "";
+            cCooldownTime = 3;
+            cLabelText = "";
+            vCooldownTime = 5;
+            vLabelText = "";
+        } else if (playerType == PlayerType.PLAYER1) {
+            xSkillTexture = new Texture("PlayerAbilities/Player1/zSkill.png");
+            cSkillTexture = new Texture("PlayerAbilities/Player1/zSkill.png");
+            vSkillTexture = new Texture("PlayerAbilities/Player1/zSkill.png");
+            zSkillTexture = new Texture("PlayerAbilities/Player1/zSkill.png");
+
+            xCooldownTime = 4;
+            xLabelText = "";
+            cCooldownTime = 3;
+            cLabelText = "";
+            vCooldownTime = 5;
+            vLabelText = "";
+        } else if (playerType == PlayerType.PLAYER2) {
+            xSkillTexture = new Texture("PlayerAbilities/Player2/cSkill.png");
+            cSkillTexture = new Texture("PlayerAbilities/Player2/cSkill.png");
+            vSkillTexture = new Texture("PlayerAbilities/Player2/cSkill.png");
+            zSkillTexture = new Texture("PlayerAbilities/Player2/cSkill.png");
+
+            xCooldownTime = 4;
+            xLabelText = "";
+            cCooldownTime = 3;
+            cLabelText = "";
+            vCooldownTime = 5;
+            vLabelText = "";
+        } else {
+            xSkillTexture = new Texture("PlayerAbilities/Player3/vSkill.png");
+            cSkillTexture = new Texture("PlayerAbilities/Player3/vSkill.png");
+            vSkillTexture = new Texture("PlayerAbilities/Player3/vSkill.png");
+            zSkillTexture = new Texture("PlayerAbilities/Player3/vSkill.png");
+
+            xCooldownTime = 4;
+            xLabelText = "";
+            cCooldownTime = 3;
+            cLabelText = "";
+            vCooldownTime = 5;
+            vLabelText = "";
+        }
+
+
+        zSkill = new Image(zSkillTexture);
+        zSkill.setSize(75, 75);
+        zSkill.setPosition(screenCenterX - 260, screenCenterY - 500);
+//        zLabel = new Label(zLabelText, labelStyleTwo);
+//        zLabel.setPosition(screenCenterX - 240, screenCenterY - 460);
+//        zLabel.addAction(Actions.alpha(0));
+//        zLabel.setFontScale(5f);
+
+
         xSkill = new Image(xSkillTexture);
         xSkill.setSize(75, 75);
         xSkill.setPosition(screenCenterX - 150, screenCenterY - 500);
+        xLabel = new Label(xLabelText, labelStyleTwo);
+        xLabel.setPosition(screenCenterX - 130, screenCenterY - 460);
+        xLabel.addAction(Actions.alpha(0));
+        xLabel.setFontScale(5f);
 
         cSkill = new Image(cSkillTexture);
         cSkill.setSize(75, 75);
         cSkill.setPosition(screenCenterX - 40, screenCenterY - 500);
+        cLabel = new Label(cLabelText, labelStyleTwo);
+        cLabel.setPosition(screenCenterX - 20, screenCenterY - 460);
+        cLabel.addAction(Actions.alpha(0));
+        cLabel.setFontScale(5f);
 
         vSkill = new Image(vSkillTexture);
         vSkill.setSize(75, 75);
         vSkill.setPosition(screenCenterX + 70, screenCenterY - 500);
+        vLabel = new Label(vLabelText, labelStyleTwo);
+        vLabel.setPosition(screenCenterX + 90, screenCenterY - 460);
+        vLabel.addAction(Actions.alpha(0));
+        vLabel.setFontScale(5f);
 
+
+        stage.addActor(zSkill);
+//        stage.addActor(zLabel);
         stage.addActor(xSkill);
+        stage.addActor(xLabel);
         stage.addActor(cSkill);
+        stage.addActor(cLabel);
         stage.addActor(vSkill);
+        stage.addActor(vLabel);
 
 
         // ------------------- FINISHED DRAWING -------------
@@ -108,8 +202,13 @@ public class GameScreen implements Screen {
 
         camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
+        random = new Random();
+        dead = new ArrayList<>();
+
+
 
     }
+
 
     @Override
     public void render(float delta) {
@@ -134,29 +233,70 @@ public class GameScreen implements Screen {
         timeStr = Double.toString(Math.round(deltaTime * 100.0) / 100.0);
         label.setText(timeStr);
 
+//        zLabelText = String.valueOf(Math.round(zCoolDownPoint + zCooldownTime - deltaTime));
+//        zLabel.setText(zLabelText);
+//        if (Integer.parseInt(zLabelText) < 0) {
+//            zLabel.addAction(Actions.alpha(0));
+//        }
+//        if (Gdx.input.isKeyJustPressed(Input.Keys.Z)) {
+//            if (deltaTime > zCoolDownPoint + zCooldownTime) {
+//                zLabel.addAction(Actions.alpha(1));
+//                zSkill.addAction(Actions.alpha(0));
+//                zSkill.addAction(Actions.fadeIn(5f));
+//                zCoolDownPoint = deltaTime;
+//            }
+//        }
+
+        xLabelText = String.valueOf(Math.round(xCoolDownPoint + xCooldownTime - deltaTime));
+        xLabel.setText(xLabelText);
+        if (Integer.parseInt(xLabelText) < 0) {
+            xLabel.addAction(Actions.alpha(0));
+        }
         if (Gdx.input.isKeyJustPressed(Input.Keys.X)) {
             if (deltaTime > xCoolDownPoint + xCooldownTime) {
+                xLabel.addAction(Actions.alpha(1));
                 xSkill.addAction(Actions.alpha(0));
-                xSkill.addAction(Actions.fadeIn(2f));
+                xSkill.addAction(Actions.fadeIn(4f));
                 xCoolDownPoint = deltaTime;
+                gonnaShake = true;
+            }
+        }
+        if (gonnaShake) {
+            if (deltaTime >= xCoolDownPoint + 2) {
+                shaking = true;
             }
         }
 
+        cLabelText = String.valueOf(Math.round(cCoolDownPoint + cCooldownTime - deltaTime));
+        cLabel.setText(cLabelText);
+        if (Integer.parseInt(cLabelText) < 0) {
+            cLabel.addAction(Actions.alpha(0));
+        }
         if (Gdx.input.isKeyJustPressed(Input.Keys.C)) {
             if (deltaTime > cCoolDownPoint + cCooldownTime) {
+                cLabel.addAction(Actions.alpha(1));
                 cSkill.addAction(Actions.alpha(0));
                 cSkill.addAction(Actions.fadeIn(3f));
                 cCoolDownPoint = deltaTime;
             }
         }
 
+
+        vLabelText = String.valueOf(Math.round(vCoolDownPoint + vCooldownTime - deltaTime));
+        vLabel.setText(vLabelText);
+        if (Integer.parseInt(vLabelText) < 0) {
+            vLabel.addAction(Actions.alpha(0));
+        }
         if (Gdx.input.isKeyJustPressed(Input.Keys.V)) {
             if (deltaTime > vCoolDownPoint + vCooldownTime) {
+                vLabel.addAction(Actions.alpha(1));
                 vSkill.addAction(Actions.alpha(0));
                 vSkill.addAction(Actions.fadeIn(5f));
                 vCoolDownPoint = deltaTime;
             }
         }
+
+
 
         if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
             Gdx.app.exit();
@@ -166,12 +306,37 @@ public class GameScreen implements Screen {
 
 
 
-
-
+        // CAMERA SHAKING OR STATIC
+        if (shaking) {
+            camera.position.x = Math.round(gameMap.getPlayer().getX()
+                    + (random.nextInt(shakeIntensityRange) - shakeIntensityBuffer));
+            camera.position.y = Math.round(gameMap.getPlayer().getY()
+                    + (random.nextInt(shakeIntensityRange) - shakeIntensityBuffer));
+            if (deltaTime >= xCoolDownPoint + 2 + shakingTime) {
+                gonnaShake = false;
+                shaking = false;
+                System.out.println("STOP IT");
+            }
+        } else {
         camera.position.x = Math.round(gameMap.getPlayer().getX());
         camera.position.y = Math.round(gameMap.getPlayer().getY());
+        }
         camera.update();
 
+
+
+//        // REMOVING THE DEAD
+//        for (Entity entity : gameMap.getEntities()) {
+//            if (entity.getLives() <= 0) dead.add(entity);
+//        }
+//        System.out.println(dead);
+//        for (Entity corpse : dead) {
+//            game.batch.draw(corpseTexture, corpse.getX(), corpse.getY());
+//        }
+//        for (Entity deadEntity : dead) {
+//            gameMap.removeEntity(deadEntity);
+//        }
+//        dead.clear();
     }
 
     @Override
---
+++ core/src/ee/taltech/iti0200/entities/Player3.java
@@ -0,0 +1,296 @@
+package ee.taltech.iti0200.entities;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.NinePatch;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import ee.taltech.iti0200.entities.Entity;
+import ee.taltech.iti0200.entities.EntityType;
+import ee.taltech.iti0200.entities.PlayerType;
+import ee.taltech.iti0200.world.GameMap;
+
+import java.util.ArrayList;
+
+
+public class Player3 extends Entity {
+
+    private static int SPEED = 80;
+    private static final int JUMP_VELOCITY = 5;
+    private static final double C_DELAY = 0.05;
+    private static final double V_DELAY = 0.75;
+    private static final double X_DELAY = 1;
+
+    private ArrayList<Entity> entities;
+
+    private Texture gunLeft, gunRight, cSkill1, cSkill2, cSkill3, xSkill1, xSkill2;
+    private NinePatch health;
+    private float totalHealth, shootingRange, lastX, lastXPos, lastC, deltaTime, cSkillX, cSkillY, lastV, lastZ, xSkillX, xSkillY;
+    private boolean isRight, shoot, moving, keyPressed, cSkill, cSkillWasRight, vSkill, vSkillSpeedUp, zSkill, xSkill,
+            bombGrounded, explosionTime;
+    private int shootingTime, movingTime, jumpingPower, cSkillRange;
+    private PlayerType playerType;
+
+    public Player3(float x, float y, GameMap map, float lives, float shootingRange, ArrayList<Entity> entities, PlayerType playerType) {
+        super(x, y, EntityType.PLAYER, map, lives);
+        this.entities = entities;
+        this.shootingRange = shootingRange;
+        this.totalHealth = getLives();
+        this.gunLeft = new Texture("gunfireleft.png");
+        this.gunRight = new Texture("gunfire.png");
+        this.lastXPos = getX();
+        this.playerType = playerType;
+        health = new NinePatch(new Texture("healthbar.png"), 0, 0, 0, 0);
+
+        this.cSkill1 = new Texture("PlayerAbilities/Player0/cSkill1.png");
+        this.cSkill2 = new Texture("PlayerAbilities/Player0/cSkill2.png");
+        this.cSkill3 = new Texture("PlayerAbilities/Player0/cSkill3.png");
+        this.xSkill1 = new Texture("PlayerAbilities/Player0/xSkill1.png");
+        this.xSkill2 = new Texture("PlayerAbilities/Player0/xSkill2.png");
+        cSkillRange = cSkill1.getWidth();
+    }
+
+    public float getLives() {
+        return this.lives;
+    }
+
+    public void jump(float deltaTime, float gravity) {
+        if (Gdx.input.isKeyPressed(Input.Keys.SPACE) && grounded) {
+            keyPressed = true;
+            jumpingPower += 1;
+        }
+        if (jumpingPower > 10 || !Gdx.input.isKeyPressed(Input.Keys.SPACE) && keyPressed && grounded) {
+            keyPressed = false;
+            this.velocityY += JUMP_VELOCITY * getWeight() * jumpingPower / 10;
+            jumpingPower = 0;
+        } else if (Gdx.input.isKeyPressed(Input.Keys.SPACE) && !grounded && this.velocityY > 0) {
+            this.velocityY += JUMP_VELOCITY * getWeight() * deltaTime;
+        }
+        super.update(deltaTime, gravity); // applies the gravity
+    }
+
+    public void moveLeft(float deltaTime) {
+        if (!Gdx.input.isKeyPressed(Input.Keys.SPACE)
+                && Gdx.input.isKeyPressed(Input.Keys.A) || Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            moveX((float) (-SPEED * 1.5 * deltaTime));
+            isRight = false;
+        } else if (Gdx.input.isKeyPressed(Input.Keys.SPACE)
+                && Gdx.input.isKeyPressed(Input.Keys.A) || Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+            moveX(-SPEED * deltaTime);
+            isRight = false;
+        }
+    }
+
+    public void moveRight(float deltaTime) {
+        if (!Gdx.input.isKeyPressed(Input.Keys.SPACE)
+                && Gdx.input.isKeyPressed(Input.Keys.D) || Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            moveX((float) (SPEED * 1.5 * deltaTime));
+            isRight = true;
+        } else if (Gdx.input.isKeyPressed(Input.Keys.SPACE)
+                && Gdx.input.isKeyPressed(Input.Keys.D) || Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+            moveX(SPEED * deltaTime);
+            isRight = true;
+        }
+    }
+
+
+    public void shoot() {
+        for (Entity entity : entities) {
+            if (Gdx.input.isKeyJustPressed(Input.Keys.Z)) {
+                shoot = true;
+                shootingTime = 0;
+                if (isRight && entity.getX() > pos.x
+                        && entity.getX() <= getX() + getWidth() + shootingRange
+                        && getY() + 0.5 * getHeight() >= entity.getY()
+                        && getY() + 0.5 * getHeight() <= entity.getY() + entity.getHeight()
+                        && entity.getLives() > 0) {
+                    entity.setLives(entity.getLives() - 1);
+                } else if (!isRight && entity.getX() < pos.x
+                        && entity.getX() + entity.getWidth() >= getX() - shootingRange
+                        && getY() + 0.5 * getHeight() >= entity.getY()
+                        && getY() + 0.5 * getHeight() <= entity.getY() + entity.getHeight()
+                        && entity.getLives() > 0) {
+                    entity.setLives(entity.getLives() - 1);
+                }
+            }
+        }
+    }
+
+
+    public void xSkill() {
+        if (Gdx.input.isKeyJustPressed(Input.Keys.X) && !xSkill) {
+            xSkill = true;
+            lastX = deltaTime;
+            xSkillX = pos.x;
+            xSkillY = pos.y;
+        }
+        if (explosionTime) {
+            for (Entity entity : entities) {
+                if (entity.type != EntityType.PLAYER &&
+                        entity.getX() >= xSkillX - 200 && entity.getX() <= xSkillX + 200
+                        && entity.getY() >= xSkillY - 200 && entity.getY() <= xSkillY + 200 ) {
+                    if (entity.getLives() >= 10) {
+                        entity.setLives(entity.getLives() - 15);
+                    } else {
+                        entity.setLives(0);
+                    }
+                }
+            }
+            explosionTime = false;
+        }
+    }
+
+
+    public void cSkill() {
+        if (Gdx.input.isKeyJustPressed(Input.Keys.C) && !cSkill) {
+            cSkill = true;
+            lastC = deltaTime;
+            cSkillX = pos.x;
+            cSkillY = pos.y;
+            cSkillWasRight = isRight;
+            for (Entity entity : entities) {
+                if (isRight && entity.getX() > pos.x
+                        && entity.getX() <= getX() + getWidth() + cSkillRange + 20
+                        && getY() + 0.5 * getHeight() >= entity.getY()
+                        && getY() + 0.5 * getHeight() <= entity.getY() + entity.getHeight()
+                        && entity.getLives() > 0) {
+                    if (entity.getLives() >= 10) {
+                        entity.setLives(entity.getLives() - 10);
+                    } else {
+                        entity.setLives(0);
+                    }
+                } else if (!isRight && entity.getX() < pos.x
+                        && entity.getX() + entity.getWidth() >= getX() - cSkillRange + 20
+                        && getY() + 0.5 * getHeight() >= entity.getY()
+                        && getY() + 0.5 * getHeight() <= entity.getY() + entity.getHeight()
+                        && entity.getLives() > 0) {
+                    if (entity.getLives() >= 10) {
+                        entity.setLives(entity.getLives() - 10);
+                    } else {
+                        entity.setLives(0);
+                    }
+                }
+            }
+        }
+    }
+
+    public void vSkill() {
+        if (Gdx.input.isKeyJustPressed(Input.Keys.V) && !vSkill) {
+            vSkill = true;
+            lastV = deltaTime;
+            SPEED += 150;
+            vSkillSpeedUp = true;
+        }
+    }
+
+
+    @Override
+    public void update(float deltaTime, float gravity) {
+        shootingTime += 1;
+        jump(deltaTime, gravity);
+        if (!keyPressed) {
+            moveLeft(deltaTime);
+            moveRight(deltaTime);
+        }
+        shoot();
+        xSkill();
+        cSkill();
+        vSkill();
+        if (shootingTime > 5) { shoot = false; }
+        if (getX() != lastXPos) {
+            movingTime += 1;
+            if (movingTime > playerType.getRunningRight().size() - 1) {
+                movingTime = 0;
+            }
+            moving = true;
+            lastXPos = getX();
+        } else {
+            moving = false;
+            movingTime = 0;
+        }
+    }
+
+    @Override
+    public void render(SpriteBatch batch) {
+        deltaTime += Gdx.graphics.getDeltaTime();
+        if (keyPressed) {
+            if (isRight) {
+                batch.draw(playerType.getRightJumpingUp(), pos.x, pos.y, getWidth(), getHeight());
+            } else batch.draw(playerType.getLeftJumpingUp(), pos.x, pos.y, getWidth(), getHeight());
+        }
+        else {
+            if (!moving || !grounded) {
+                if (isRight) {
+                    batch.draw(playerType.getStandingRight(), pos.x, pos.y, getWidth(), getHeight());
+                } else batch.draw(playerType.getStandingLeft(), pos.x, pos.y, getWidth(), getHeight());
+            } else {
+                if (isRight) {
+                    batch.draw(playerType.getRunningRight().get(movingTime), pos.x, pos.y, getWidth(), getHeight());
+                } else batch.draw(playerType.getRunningLeft().get(movingTime), pos.x, pos.y, getWidth(), getHeight());
+            }
+        }
+        health.draw(batch, pos.x, pos.y + 40, (getLives() / this.totalHealth) * getWidth(), 3);
+
+        if (shoot) {
+            if (isRight) {
+                batch.draw(gunRight, pos.x + getWidth(), pos.y + getHeight() / 4, 5, 5);
+            } else {
+                batch.draw(gunLeft, pos.x - 5, pos.y + getHeight() / 4, 5, 5);
+            }
+        }
+
+        if (xSkill) {
+            if (deltaTime <= lastX + X_DELAY){
+                if (!map.doesRectCollideMap(xSkillX, xSkillY - 2, xSkill1.getWidth(), xSkill1.getHeight())) {
+                    batch.draw(xSkill1, xSkillX, xSkillY -= 2);
+                } else {
+                    batch.draw(xSkill1, xSkillX, xSkillY);
+                }
+            } else if (deltaTime > lastX + X_DELAY && deltaTime <= lastX + X_DELAY * 2) {
+                if (!map.doesRectCollideMap(xSkillX, xSkillY - 2, xSkill2.getWidth(), xSkill2.getHeight())) {
+                    batch.draw(xSkill2, xSkillX, xSkillY -= 2);
+                } else {
+                    batch.draw(xSkill2, xSkillX, xSkillY);
+                }
+            } else if (deltaTime > lastX + X_DELAY * 2 && deltaTime <= lastX + 4 ) {
+                explosionTime = true;
+            }
+            else if (deltaTime > lastX + 4) {
+                xSkill = false;
+            }
+        }
+
+
+        if (cSkill) {
+            if (cSkillWasRight) {
+                if (deltaTime <= lastC + C_DELAY) {
+                    batch.draw(cSkill1, pos.x + 20, pos.y, 200, 24);
+                } else if (deltaTime > lastC + C_DELAY && deltaTime <= lastC + C_DELAY * 2) {
+                    batch.draw(cSkill2, pos.x + 20, pos.y, 200, 24);
+                } else if (deltaTime > lastC + C_DELAY * 2 && deltaTime <= lastC + C_DELAY * 3) {
+                    batch.draw(cSkill3, pos.x + 20, pos.y, 200, 24);
+                } else if (deltaTime >= lastC + 3) {
+                    cSkill = false;
+                }
+            } else {
+                if (deltaTime <= lastC + C_DELAY) {
+                    batch.draw(cSkill1, pos.x - 210, pos.y, 200, 24);
+                } else if (deltaTime > lastC + C_DELAY && deltaTime <= lastC + C_DELAY * 2) {
+                    batch.draw(cSkill2, pos.x - 210, pos.y, 200, 24);
+                } else if (deltaTime > lastC + C_DELAY * 2 && deltaTime <= lastC + C_DELAY * 3) {
+                    batch.draw(cSkill3, pos.x - 210, pos.y, 200, 24);
+                } else if (deltaTime >= lastC + 3) {
+                    cSkill = false;
+                }
+            }
+        }
+        if (vSkill) {
+            if (vSkillSpeedUp && deltaTime >= lastV + V_DELAY) {
+                SPEED -= 150;
+                vSkillSpeedUp = false;
+            }
+            if (deltaTime >= lastV + 5) vSkill = false;
+        }
+    }
+
+}
