f34818920ef15c35f30c824da4e39fef8320e9e4
pikoiv <pikoiv@ttu.ee>
2020-04-22 11:29:34 +0300

    Issue 49

--- desktop/src/ee/taltech/iti0200/desktop/DesktopLauncher.java
+++ desktop/src/ee/taltech/iti0200/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package ee.taltech.iti0200.desktop;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.esotericsoftware.kryo.Kryo;
